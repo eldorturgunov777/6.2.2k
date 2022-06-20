561d3444d6bb56ba71976393008a4761d5575cae
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-06-20 17:02:34 +0500

    new

--- app/src/main/java/com/example/a622k/MainActivity.kt
+++ app/src/main/java/com/example/a622k/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.a622k
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.a622k.manager.PrefsManager
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private var edit_email: EditText? = null
     private var edit_password: EditText? = null
     private var edit_passwordConfirm: EditText? = null
+    private var load_name: TextView? = null
+    private var load_email: TextView? = null
+    private var load_psw: TextView? = null
+    private var load_pswC: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +26,49 @@ class MainActivity : AppCompatActivity() {
         edit_email = findViewById(R.id.edit_email);
         edit_password = findViewById(R.id.edit_password);
         edit_passwordConfirm = findViewById(R.id.edit_passwordConfirm);
+        load_name = findViewById(R.id.load_name)
+        load_email = findViewById(R.id.load_email)
+        load_psw = findViewById(R.id.load_psw)
+        load_pswC = findViewById(R.id.load_pswC)
+        save();
+        load();
     }
 
-    fun button_register(view: View) {
-        val name = edit_name!!.text.toString()
-        val email = edit_email!!.text.toString()
-        val psw = edit_password!!.text.toString()
-        val psw_conf = edit_passwordConfirm!!.text.toString()
+    @SuppressLint("SetTextI18n")
+    private fun load() {
+        val button_load = findViewById<Button>(R.id.button_load)
+        button_load.setOnClickListener { view: View? ->
+            val prefs = applicationContext.getSharedPreferences(
+                "MyPref",
+                MODE_PRIVATE
+            )
+            val name = prefs.getString("name", null)
+            val email = prefs.getString("email", null)
+            val psw = prefs.getString("psw", null)
+            val psw_c = prefs.getString("psw_c", null)
+            if (name != null || email != null || psw != null || psw_c != null) {
+                load_name!!.text = "Full name: $name"
+                load_email!!.text = "Email : $email"
+                load_psw!!.text = "Password : $psw"
+                load_pswC!!.text = "Password Confirm : $psw_c"
+            }
+        }
+    }
 
-        PrefsManager.getInstance(this)!!.saveData("name", name)
-        PrefsManager.getInstance(this)!!.saveData("email", email)
-        PrefsManager.getInstance(this)!!.saveData("psw", psw)
-        PrefsManager.getInstance(this)!!.saveData("psw_conf", psw_conf)
+    private fun save() {
+        val button_register = findViewById<Button>(R.id.button_register)
+        button_register.setOnClickListener { view: View? ->
+            val prefs = applicationContext.getSharedPreferences(
+                "MyPref",
+                MODE_PRIVATE
+            )
+            val editor = prefs.edit()
+            editor.putString("name", edit_name!!.text.toString())
+            editor.putString("email", edit_email!!.text.toString())
+            editor.putString("psw", edit_password!!.text.toString())
+            editor.putString("psw_c", edit_passwordConfirm!!.text.toString())
+            editor.apply()
+        }
+
     }
 }
